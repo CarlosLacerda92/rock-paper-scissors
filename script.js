d30f3622b663ea9b91f6c63d7d8665309eab4a88
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-03-08 18:22:26 -0300

    Perf: optimize getComputerChoice() function
    The getComputerChoice() had too many unnecessary conditions. The whole
    function has been rewritten using an array, which made the code shorter
    and cleaner and improved its readability.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const getComputerChoice = () => {
+/* 
+First logic.
+It works, but it has unnecessary conditional checks.
+*/
+/* const getComputerChoice = () => {
 
     const num = Math.floor((Math.random() * 9));
 
@@ -15,4 +19,12 @@ const getComputerChoice = () => {
     }
 
     return null;
+} */
+
+//  Optimized and cleaner function
+const getComputerChoice = () => {
+
+    const choices      = ["rock", "paper", "scissors"];                 //  I actually thought using an array would be a better option for this case.
+    const randomNumber = Math.floor(Math.random() * choices.length);    //  But I didn't think I could generate a random number
+    return choices[randomNumber];                                       //  and use it as an index to return the corresponding item from the choices array.
 }
